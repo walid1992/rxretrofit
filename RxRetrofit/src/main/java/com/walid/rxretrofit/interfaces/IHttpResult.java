540ae235b8f4211883a1fbcdcdf181ac35400916
walid <walid@waliddeMBP.lan>
2016-09-02 18:51:57 +0800

    提交完整代码

---
+++ RxRetrofit/src/main/java/com/walid/rxretrofit/interfaces/IHttpResult.java
@@ -0,0 +1,17 @@
+package com.walid.rxretrofit.interfaces;
+
+/**
+ * Author   : walid
+ * Data     : 2016-08-18  15:59
+ * Describe : IHttpResult
+ */
+public interface IHttpResult<T> {
+
+    int getCode();
+
+    String getMsg();
+
+    T getData();
+
+}
+
