21b89b7240421cfd004605c3c97e6b9716f7a5c7
walid1992 <541280314@qq.com>
2016-10-20 19:49:40 +0800

    提交log

--- app/src/main/java/com/walid/rxretrofitsample/MainActivity.java
+++ app/src/main/java/com/walid/rxretrofitsample/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // dev log
         tvContent = (TextView) findViewById(R.id.tv_content);
         InsService.list(new SimpleHttpCallback<List<InsuranceVo>>() {
             @Override
